b3e9b0fc839a1040e5fe1e558bf9ec36c21b5dfd
Jayshiv Patel <jdp221@ic.ac.uk>
2024-07-30 22:12:22 +0100

    Refactor out synonym

---
+++ src/pages/components/SynonymChip.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import { Chip } from 'react-native-paper';
+
+export const SynonymChip = ({label, items}) => (
+    <View style={{ flexDirection: 'row', flexWrap: 'wrap', marginVertical: 5 }}>
+      {items.map((item, index) => (
+        <Chip key={`${label}-${index}`} style={{ margin: 2 }}>
+          {item}
+        </Chip>
+      ))}
+    </View>
+);
